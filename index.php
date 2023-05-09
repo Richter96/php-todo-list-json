737706a4afdb134c905a1aa66e0390cb15a3db9a
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-09 12:51:46 +0200

    add fontawesone

--- index.php
+++ index.php
@@ -50,10 +50,13 @@ file_put_contents('todoList.json', $new_todo_list_string);
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./style.css">
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="./style.css">
 
 </head>
 
@@ -67,10 +70,15 @@ file_put_contents('todoList.json', $new_todo_list_string);
             <div class="col-5 text-center m-auto">
                 <input type="text" v-model="new_item_list" @keyup.enter="add_item" placeholder="Inserisci un nuovo elemento">
             </div>
-            <div class="card col-4 listItems">
+            <div class="card col-4 my-3 listItems">
                 <ul class="list-group list-group-flush mx-5">
-                    <li :class="list.done ? 'text-decoration-line-through' : ''" class="list-group-item" v-for="(list, index) in lists_todo" @click="complet_task(index)">
-                        {{ list.text }}
+                    <li :class="list.done ? 'text-decoration-line-through' : ''" class="list-group-item list-group-item-action d-flex justify-content-between" v-for="(list, index) in lists_todo" @click="complet_task(index)">
+                        <p class="m-0">
+                            {{ list.text }}
+                        </p>
+                        <a class="trash" href="">
+                            <i class="fa-solid fa-trash-can"></i>
+                        </a>
                     </li>
                 </ul>
             </div>
